f719d8575c6cff330a4b5af6825272596b09f36e
hellooe <hellooe@qq.com>
2021-01-20 09:25:14 +0800

    use yaml as config

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,9 @@
-use clap::{value_t_or_exit, App, Arg, ArgMatches, SubCommand};
+use clap::{load_yaml, value_t_or_exit, App, ArgMatches};
 use tour::cmd;
 
 fn main() {
-    let app = App::new("tour")
-        .about("做一些有趣的事")
-        .subcommand(
-            SubCommand::with_name("word")
-                .about("转化格式")
-                .arg(
-                    Arg::with_name("case")
-                        .short("c")
-                        .long("case")
-                        .help("请选择转换模式")
-                        .takes_value(true)
-                        .default_value("snake"),
-                )
-                .arg(
-                    Arg::with_name("input")
-                        .short("i")
-                        .long("input")
-                        .help("请输入单词内容")
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("time")
-                .about("计算时间")
-                .arg(
-                    Arg::with_name("weeks")
-                        .short("w")
-                        .long("weeks")
-                        .help("输入周数")
-                        .takes_value(true)
-                        .conflicts_with("timezone"),
-                )
-                .arg(
-                    Arg::with_name("timezone")
-                        .short("t")
-                        .long("timezone")
-                        .help("输入时区")
-                        .takes_value(true),
-                ),
-        )
-        .get_matches();
+    let yaml = load_yaml!("tour.yaml");
+    let app = App::from_yaml(yaml).get_matches();
 
     match app.subcommand() {
         ("word", Some(m)) => word_cmd(m),
